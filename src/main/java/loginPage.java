e1af1966cf88efadf2850ef0276b64e7338205d8
mahmoud <mahmoudali01>
2022-05-11 23:29:34 +0200

    page factory

--- src/main/java/loginPage.java
+++ src/main/java/loginPage.java
@@ -2,13 +2,17 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class loginPage {
     private WebDriver driver;
     public loginPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver,this);
     }
-
+//    @FindBy(id= "username")
+//    WebElement usernamePF;
     WebElement usernameElementPOM(){
 //        By username = By.id("username");
 //        WebElement usernameEle = driver.findElement(username);
@@ -32,6 +36,8 @@ public class loginPage {
 
         usernameElementPOM().clear();
         usernameElementPOM().sendKeys(username);
+//        usernamePF.clear();
+//        usernamePF.sendKeys(username);
         passElementPOM().sendKeys(password);
         passElementPOM().sendKeys(Keys.ENTER);
 
